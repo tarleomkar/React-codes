3c1539dbefb73c6f73acd0a6e28e76313c2fcb52
Omkar Tarle <tarleomkar8@gmail.com>
2025-10-13 22:52:09 +0530

    destructuring the props

--- 04_cards_project/src/components/Card.jsx
+++ 04_cards_project/src/components/Card.jsx
@@ -1,25 +1,25 @@
 import { Bookmark } from 'lucide-react'
-const Card = (props) => {
+const Card = ({companyName, datePosted, post, tag1, brandLogo, pay, tag2}) => {
 
     return (
         <div className="card">
             <div>
                 <div className="top">
-                    <img src={props.brandLogo} alt="nachioooooo" />
+                    <img src={brandLogo} alt="nachioooooo" />
                     <button>Save <Bookmark size={10} /> </button>
                 </div>
                 <div className="center">
-                    <h3>{props.company} <span>{props.datePosted}</span></h3>
-                    <h2>{props.post}</h2>
+                    <h3>{companyName} <span>{datePosted}</span></h3>
+                    <h2>{post}</h2>
                     <div className='tag'>
-                        <h4>{props.tag1}</h4>
-                        <h4>{props.tag2}</h4>
+                        <h4>{tag1}</h4>
+                        <h4>{tag2}</h4>
                     </div>
                 </div>
             </div>
             <div className="bottom">
                 <div>
-                    <h3>{props.pay}</h3>
+                    <h3>{pay}</h3>
                     <p>Mumbai, India</p>
                 </div>
                 <button>Apply Now</button>
